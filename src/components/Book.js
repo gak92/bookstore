e1107a8b2a2893517b32378fdf7736e856995d92
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-12 00:14:13 +0500

    Display book list using Book Component, using props to display title and author

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,35 @@
 import React from "react";
 
-const Book = () => {
+const Book = (props) => {
+  const { id, title, author, category } = props;
+
   return (
     <div className="book-div">
-      Book 1
+
+      <div className="book-info">
+        <div>
+          <p>{category}</p>
+          <h3>{title}</h3>
+          <p>{author}</p>
+        </div>
+        <div className="book-btns">
+          <button type="button">Comments</button>
+          <button type="button">Remove</button>
+          <button type="button">Edit</button>
+        </div>
+      </div>
+
+      <div className="book-status">
+        <div className="status-circle"></div>
+        <p>64%</p>
+        <p>Completed</p>
+      </div>
+
+      <div className="book-chapter">
+        <p>Current chapter</p>
+        <p>chapter 17</p>
+        <button type="button">Update Progress</button>
+      </div>
     </div>
   );
 };
