15903299f9bd52f5c5947cca4a99b48d213a1dd7
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-17 00:23:41 +0500

    Add functionality to remove book from the list

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -33,6 +33,7 @@ const BooksList = () => {
         books.map((book) => (
           <Book
             key={book.id}
+            id={book.id}
             title={book.title}
             author={book.author}
             category={book.category}
