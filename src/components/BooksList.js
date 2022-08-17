330151fab1195551877041033e3434ba4d414032
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-18 01:21:01 +0500

    Update addBook and removeBook functionality to use api

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './Book';
+import { displayBooks } from '../redux/books/books';
 
 const BooksList = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(displayBooks());
+  }, []);
+
   const books = useSelector((state) => state.books);
+  console.log(books);
 
   return (
     <section className="book-list">
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -7,31 +7,34 @@ const requestedURL = `${baseURL}/apps/${apiKey}/books`;
 // ACTIONS
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const DISPLAY_BOOKS = 'bookstore/books/DISPLAY_BOOKS';
 
 const initialState = [
-  {
-    id: '1',
-    title: 'The hunger games',
-    author: 'Suzanne Collins',
-    category: 'Action',
-  },
-  {
-    id: '2',
-    title: 'Dune',
-    author: 'Frank Herbert',
-    category: 'Science Fiction',
-  },
-  {
-    id: '3',
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    category: 'Economy',
-  },
+  // {
+  //   id: '1',
+  //   title: 'The hunger games',
+  //   author: 'Suzanne Collins',
+  //   category: 'Action',
+  // },
+  // {
+  //   id: '2',
+  //   title: 'Dune',
+  //   author: 'Frank Herbert',
+  //   category: 'Science Fiction',
+  // },
+  // {
+  //   id: '3',
+  //   title: 'Capital in the Twenty-First Century',
+  //   author: 'Suzanne Collins',
+  //   category: 'Economy',
+  // },
 ];
 
 // REDUCER
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
+    case DISPLAY_BOOKS:
+      return action.payload;
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
@@ -42,6 +45,26 @@ const booksReducer = (state = initialState, action) => {
 };
 
 // ACTIONS CREATOR
+export const displayBooks = () => async (dispatch) => {
+  const response = await fetch(requestedURL);
+  const res = await response.json();
+  const books = Object.entries(res).map(([key, val]) => {
+    const { title, author, category } = val[0];
+    return {
+      id: key,
+      title,
+      author,
+      category,
+    };
+  });
+  if (books) {
+    dispatch({
+      type: DISPLAY_BOOKS,
+      payload: books,
+    });
+  }
+};
+
 export const addBook = (title, author) => async (dispatch) => {
   const book = {
     item_id: uuidv4(),
@@ -61,11 +84,24 @@ export const addBook = (title, author) => async (dispatch) => {
     }));
 };
 
-export const removeBook = (id) => ({
-  type: REMOVE_BOOK,
-  payload: {
-    id,
-  },
-});
+export const removeBook = (id) => async (dispatch) => {
+  const url = `${requestedURL}/${id}`;
+  console.log('URL: ', url);
+  console.log('ID: ', id);
+  const response = await fetch(url, {
+    method: 'DELETE',
+    body: JSON.stringify({ item_id: id }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  console.log('Response: ', response);
+  dispatch({
+    type: REMOVE_BOOK,
+    payload: {
+      id,
+    },
+  });
+};
 
 export default booksReducer;
