5455511fba3829a5a585bf2e291bfabecb8f68aa
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-17 19:37:41 +0500

    Update addBook to add new book via API call

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,9 @@
 import { v4 as uuidv4 } from 'uuid';
 
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+const apiKey = 'vEEu5G5Iqrs3wYfAbg4u';
+const requestedURL = `${baseURL}/apps/${apiKey}/books`;
+
 // ACTIONS
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
@@ -38,14 +42,24 @@ const booksReducer = (state = initialState, action) => {
 };
 
 // ACTIONS CREATOR
-export const addBook = (title, author) => ({
-  type: ADD_BOOK,
-  payload: {
-    id: uuidv4(),
+export const addBook = (title, author) => async (dispatch) => {
+  const book = {
+    item_id: uuidv4(),
     title,
     author,
-  },
-});
+    category: 'Action',
+  };
+
+  await fetch(requestedURL, {
+    method: 'POST',
+    body: JSON.stringify(book),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  })
+    .then(() => dispatch({
+      type: ADD_BOOK,
+      payload: book,
+    }));
+};
 
 export const removeBook = (id) => ({
   type: REMOVE_BOOK,
